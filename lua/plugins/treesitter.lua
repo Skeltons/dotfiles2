165d96f060cdfcf13d1ebeb1d4a1bb2d84373630
Oskar Lew <oskarlew98@gmail.com>
2024-11-16 00:16:14 +0800

    Add auto install

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,7 +7,9 @@ return {
             },
             indent = {
                 enable = true
-            }
+            },
+            auto_install = true,
+            additional_vim_regex_highlighting = false,
         }
         vim.treesitter.language.register('html', 'jsp')
     end,
