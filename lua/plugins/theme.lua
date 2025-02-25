b5c9959abfebc15ddcc9a6714f9a58b9b9276d4e
Oskar <oskarlew98@gmail.com>
2025-02-25 22:36:47 +0800

    Change theme setting

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,13 +3,25 @@ return {
         "folke/tokyonight.nvim",
         lazy = false,
         priority = 1000,
-        opts = {},
-
+        opts = {
+            transparent = true,
+            styles = {
+                sidebars = "transparent",
+                floats = "transparent",
+            }
+        },
     }, {
         "navarasu/onedark.nvim",
         lazy = false,
         priority = 1000,
         opts = {}
+    }, {
+        "ellisonleao/gruvbox.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {
+            transparent_mode = true,
+        }
     }
 }
 
